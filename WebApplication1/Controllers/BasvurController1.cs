6c72af1ea8457ebd7bcc8c6fa8da2cb33c05b7c3
nureddin9932 <154005604+nureddin9932@users.noreply.github.com>
2024-07-10 23:48:20 -0700

    basavur/yenileme

---
+++ WebApplication1/Controllers/BasvurController1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.data;
+using WebApplication1.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    public class BasvurController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BasvurController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Basvur()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Basvur(personel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Personeller
+                    .FirstOrDefaultAsync(u => u.TcKimlikNumarasi == model.TcKimlikNumarasi && u.Password == model.Password);
+
+                if (user != null)
+                {
+                    // Successful login
+                    return RedirectToAction("Success");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Incorrect username or password");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/yenilemeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.data;
+using WebApplication1.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    public class yenilemeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public yenilemeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult yenileme()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(personel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Personeller
+                    .FirstOrDefaultAsync(u => u.TcKimlikNumarasi == model.TcKimlikNumarasi && u.Password == model.Password);
+
+                if (user != null)
+                {
+                    // Successful login
+                    return RedirectToAction("Success");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Incorrect username or password");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
